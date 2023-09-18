0c4a7ae8502fe6ac9394dad99481fd2b80f4632a
munqhar <hernan_ortega94@hotmail.com>
2023-09-18 14:27:20 -0500

    Guess My Number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,43 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    { string answer = "yes";
+        while (answer == "yes")
+        {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);  
+        int count=0;                             // generating the random number for the magic number
+        /* Console.WriteLine("What is the magic number?");
+        string answer =  Console.ReadLine();
+        int number = int.Parse(answer); */
+        int guess = 0;
+        
+        
+        
+        do
+        {
+         Console.WriteLine("What is your guess?");
+         string guessString = Console.ReadLine();                               //saving the guess number we use a do-while loop
+         guess= int.Parse(guessString);
+         if (guess < number)
+         {
+            Console.WriteLine("higher");
+         }
+         else if (guess > number)
+         {
+            Console.WriteLine("lower");
+         }
+         else 
+         {
+               Console.WriteLine("you guess the number");
+         }
+         count++;
+         
+        }
+        while(guess != number);
+        Console.WriteLine($"the number of tries were {count}");
+        Console.WriteLine("Do you want to continue playing yes or not ?");
+        answer = Console.ReadLine();
+        }
     }
 }
