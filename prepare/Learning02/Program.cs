535af8dabd089a3938bb2f2c7f24eeeb4979b35c
munqhar <hernan_ortega94@hotmail.com>
2023-09-26 19:57:01 -0500

    working2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company ="Oso tedy";
+        job1._jobTitle = "salesman";
+        job1._startYear = 2002;
+        job1._endYear = 2003;
+        job1.Display();
+        job1.DisplayJobDetails();
+        
+        Job job2 = new Job();
+        job2._company ="Coca-Cola";
+        job2._jobTitle = "Business administrator";
+        job2._startYear = 2012;
+        job2._endYear = 2025;
+        job2.Display();
+        job2.DisplayJobDetails();
+        Resume resume = new Resume();
+        
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,16 @@
+public class Resume
+  {         
+           public string _name ="";
+           public List<Job> _jobs;
+           
+            public void Display()
+         {
+            
+            Console.WriteLine($"{_name}");
+        
+         }
+      
+
+
+
+}
