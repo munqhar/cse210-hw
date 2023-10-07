579f57a4ca6870f710ead3c25740fd1b455fe28f
munqhar <hernan_ortega94@hotmail.com>
2023-10-07 16:46:11 -0500

    Fuctional Requierments of the journal

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,6 +2,7 @@ public class Entry
 { public string _response;
 public string _datetext;
 public string Prompt;
+public string  eachentries;
 
     public string showpromptAndSaveEntry()
     {
@@ -14,7 +15,13 @@ public string Prompt;
     }
     public void Display ()
     {
-        Console.WriteLine($"The Prompt: <<{Prompt}>>\nThe response: <<{_response}>>\nThe date: {_datetext}");
+        Console.WriteLine($"The Prompt: {Prompt}\nThe response: {_response}\nThe date: {_datetext}");
+    }
+    public string Eachentries()
+    {
+       eachentries = ($"{Prompt},,{_response},,{_datetext}");
+       return eachentries;
+
     }
 
 
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,30 +1,32 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
         
-         Entry FirstEntry = new Entry();
-         PromptGenerator PromptGenerator = new PromptGenerator();
-         FirstEntry.Prompt=PromptGenerator.CreatesPrompt();
+          
          string _numberMenu=" ";
          Journal journal = new Journal();
          int i=0;
+         string _fileName;
        do
        {
          Console.Write("Welcome to the Journal Program! \nPlease select on of the following choices \n1.Write \n2.Display \n3.Load\n4.Save\n" );
          Console.WriteLine("5.Quit\nWhat would you like to do? ");
           _numberMenu = Console.ReadLine();
-          journal._entries[i] = FirstEntry ;
+          //journal._entries[i] = FirstEntry ;
          switch (_numberMenu)
          {
              case "1":           
-             
+             Entry FirstEntry = new Entry(); //FirstEntry is encharged to show and save the prompt, the response and the date. 
+             PromptGenerator PromptGenerator = new PromptGenerator();
+             FirstEntry.Prompt=PromptGenerator.CreatesPrompt();
              FirstEntry._response=FirstEntry.showpromptAndSaveEntry();
              DateTime theCurrentTime = DateTime.Now;
              FirstEntry._datetext = theCurrentTime.ToShortDateString();
-             
+             journal._entries.Add(FirstEntry);              
              
              break;
  
@@ -32,8 +34,31 @@ class Program
               journal.Display();
              break;
              case "3":
+                  
+             Console.WriteLine("What is the filename?");
+             _fileName = Console.ReadLine();
+             string[] lines = System.IO.File.ReadAllLines(_fileName);
+             foreach (string line in lines)
+             {Entry SecondEntry = new Entry();  // the second entry is encharged to load the information in a file 
+              string[] parts = line.Split(",,");
+                SecondEntry.Prompt = parts[0];             
+                SecondEntry._response = parts[1];             
+                SecondEntry._datetext = parts[2];   
+                journal._entries.Add(SecondEntry);           
+              
+             }
+             break;
  
              case "4":
+             Console.WriteLine("What is the filename?");
+             _fileName = Console.ReadLine();
+             using (StreamWriter outputFile = new StreamWriter(_fileName))
+             {foreach ( Entry entry in journal._entries)
+              {          
+               outputFile.WriteLine(entry.Eachentries());          
+              }             
+             }
+             break;
  
              case "5":
              break;
