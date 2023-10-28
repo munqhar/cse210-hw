ac947d300834dc4efacd7b9941189b59abe7d156
munqhar <hernan_ortega94@hotmail.com>
2023-10-28 13:39:02 -0500

    Diagrams

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        
+    
+DateTime startTime = DateTime.Now;
+DateTime futureTime = startTime.AddSeconds(10);
+int i = 0;
+while(i == 0)
+{ 
+Thread.Sleep(2000);
+
+DateTime currentTime = DateTime.Now;
+if (currentTime < futureTime)
+{
+    Console.WriteLine("We have not arrived at our future time yet...");
+    
+}
+else{
+    Console.WriteLine("We have arrived");  
+    break;
+    }  
+ }
+
     }
 }
