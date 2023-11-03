da34a8607414b51ee4484678a1bf624452f260be
munqhar <hernan_ortega94@hotmail.com>
2023-11-03 10:51:26 -0500

    finish the normal requirements

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,28 +3,40 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   string _numberMenu;
         do
          { 
-            Console.Write("Welcome to the Journal Program! \nPlease select on of the following choices \n1.Write \n2.Display \n3.Load\n4.Save\n" );
-            Console.WriteLine("5.Quit\nWhat would you like to do? ");
-            _numberMenu = Console.ReadLine();
-            switch (_numberMenu)          
-             Breathing Breath = new Breathing();
-             Breath.DisplayTheStartingMessage();        
-             Console.WriteLine("");
-             Breath.RunBreathingActivity();         
-             Breath.DisplayTheEndingMessage();  
-         
-             Reflection reflection = new Reflection();
-             reflection.DisplayTheStartingMessage(); 
-             reflection.RunReflectingActivity();
-             reflection.DisplayTheEndingMessage();      
-       
-             Listing listing = new Listing();
-             listing.DisplayTheStartingMessage();
-             listing.RunListingActivity();
-             listing.DisplayTheEndingMessage();
-         }
+             Console.Write("Menu Options:\n1. Start Breathing Activity \n2. Start Reflecting Activity \n3. Start Listing Activity \n4. Quit\n");
+             Console.Write("Select a choice from the menu: ");
+              _numberMenu = Console.ReadLine();
+             switch (_numberMenu)          
+             {
+              case "1":
+              Breathing Breath = new Breathing();
+              Breath.DisplayTheStartingMessage();        
+              Console.WriteLine("");
+              Breath.RunBreathingActivity();         
+              Breath.DisplayTheEndingMessage();
+              Console.Clear();
+              break;  
+
+              case "2":
+              Reflection reflection = new Reflection();
+              reflection.DisplayTheStartingMessage(); 
+              reflection.RunReflectingActivity();
+              reflection.DisplayTheEndingMessage(); 
+              Console.Clear();
+              break;   
+
+              case "3":
+              Listing listing = new Listing();
+              listing.DisplayTheStartingMessage();
+              listing.RunListingActivity();
+              listing.DisplayTheEndingMessage();
+              Console.Clear();
+              break;
+              
+             }
+         }while (_numberMenu != "4");
     }
 }
