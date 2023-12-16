fb8377a724766191f8fca7c5170a51ccbdb0e8fe
munqhar <hernan_ortega94@hotmail.com>
2023-12-16 14:57:49 -0500

    I fixxed the mistakes

---
+++ Develop05/EternalGoal.cs
@@ -0,0 +1,37 @@
+public class EternalGoal: Goal
+{
+    
+    public EternalGoal(string name, string description, int points, string goalType ): base( name,  description,  points, goalType )
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _goalType = goalType;
+//        _listGoal = listGoal;
+        
+    }
+    public override float RecordEvents()
+    {
+     Console.WriteLine($"Congratulations! You have earned {_points} points!");               
+     _isComplete = true;
+     return _points;
+        
+    }
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+    public void set(string name, string description, int points, string goalType)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _goalType = goalType;
+
+    }
+      public override void ShowListGoal()
+    {
+        Console.WriteLine($"{_name} ({_description})");
+    }
+   
+}
---
+++ Develop05/SimpleGoal.cs
@@ -0,0 +1,39 @@
+public class SimpleGoal: Goal
+{
+    //protected string _goalType = "Simple Goal";
+    public SimpleGoal(string name, string description, int points, string goalType ): base( name,  description,  points, goalType )
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _goalType = goalType;
+//        _listGoal = listGoal;
+        
+    }
+    
+    public override float RecordEvents()
+    {
+     Console.WriteLine($"Congratulations! You have earned {_points} points!");               
+     _isComplete = true;
+     return _points;
+    }
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+    public void set(string name, string description, int points, string goalType)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _goalType = goalType;
+        //_listGoal = listGoal;
+
+    }
+     public override void ShowListGoal()
+    {
+        Console.WriteLine($"{_name} ({_description})");
+    }
+   
+
+}
